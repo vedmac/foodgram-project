fc55855c201abcd8072d09a5129b48df6b37d984
Viktor Ermolov <viktor@ermolov.dev>
2021-04-19 02:16:00 +0000

    fix settings

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -139,16 +139,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_URL = '/static/'
+
 if DEBUG:
-    STATIC_URL = '/static/'
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static"),
     ]
 else:
-    STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 LOGIN_URL = '/auth/login/'
